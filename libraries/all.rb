e6aa2118834aba3212ddf6b2718dd1fc829e201b
Igor Rzegocki <ajgon@irgon.com>
2016-04-10 12:07:44 +0200

    Moved libraries to flat directory structure, because AWS chef hates us

--- libraries/all.rb
+++ libraries/all.rb
@@ -2,6 +2,7 @@
 libdir = File.expand_path('..', __FILE__)
 require File.join(libdir, 'core_ext')
 require File.join(libdir, 'helpers')
-Dir[File.join(libdir, 'drivers', 'dsl', '**', '*.rb')].each { |f| require f }
-Dir[File.join(libdir, '*', '**', 'base.rb')].each { |f| require f }
-Dir[File.join(libdir, '*', '**', '*.rb')].each { |f| require f }
+Dir[File.join(libdir, 'drivers_dsl_*.rb')].each { |f| require f }
+Dir[File.join(libdir, 'drivers_base.rb')].each { |f| require f }
+Dir[File.join(libdir, 'drivers_*_base.rb')].each { |f| require f }
+Dir[File.join(libdir, '*.rb')].each { |f| require f }
