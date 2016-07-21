3eb8c5fd6a16a429035fd38fd95e95c2391f1e22
Igor Rzegocki <ajgon@irgon.com>
2016-07-21 20:10:33 +0200

    chore: Removed hardcoded `build-essentials` dependency
    Closes #23

--- metadata.rb
+++ metadata.rb
@@ -7,7 +7,7 @@ description 'Set of chef recipes for OpsWorks based Ruby projects'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '0.4.0'
 
-depends 'build-essential', '~> 2.0'
+depends 'build-essential'
 depends 'deployer'
 depends 'ruby-ng'
 depends 'nginx', '~> 2.7'
