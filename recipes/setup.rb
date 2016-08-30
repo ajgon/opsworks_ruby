e2c42b43c725302b70ed19c715294de9d45e8143
Kevin Olbrich <kevin.olbrich@mckesson.com>
2016-08-30 10:33:22 -0400

    fix: install bundler for system ruby

--- recipes/setup.rb
+++ recipes/setup.rb
@@ -14,23 +14,29 @@ if node['ruby']['install_method'] == 'ruby_build'
   ruby_build_ruby node['ruby-ng']['ruby_version'] do
     prefix_path '/usr/local'
   end
+  gem_package 'bundler' do
+    gem_binary '/usr/local/bin/gem'
+  end
 elsif node['platform_family'] == 'debian'
   include_recipe 'ruby-ng::dev'
+  gem_package 'bundler'
 else
   ruby_pkg_version = node['ruby-ng']['ruby_version'].split('.')[0..1]
   package "ruby#{ruby_pkg_version.join('')}"
   package "ruby#{ruby_pkg_version.join('')}-devel"
   execute "/usr/sbin/alternatives --set ruby /usr/bin/ruby#{ruby_pkg_version.join('.')}"
+  gem_package 'bundler'
 end
 
-gem_package 'bundler'
 if node['platform_family'] == 'debian'
   link '/usr/local/bin/bundle' do
     to '/usr/bin/bundle'
+    not_if { ::File.exist?('/usr/local/bin/bundle') }
   end
 else
   link '/usr/local/bin/bundle' do
     to '/usr/local/bin/bundler'
+    not_if { ::File.exist?('/usr/local/bin/bundle') }
   end
 end
 every_enabled_application do |application, _deploy|
