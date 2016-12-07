a2a2b5353a836000a529f91098465e6c1d7afb44
Kevin Pheasey <kevin@kpheasey.com>
2016-12-07 11:37:22 -0500

    add aurora as allowed engine for MySQL driver

--- libraries/drivers_db_mysql.rb
+++ libraries/drivers_db_mysql.rb
@@ -3,7 +3,7 @@ module Drivers
   module Db
     class Mysql < Base
       adapter :mysql2
-      allowed_engines :mysql, :mysql2, :mariadb
+      allowed_engines :mysql, :mysql2, :mariadb, :aurora
       packages debian: 'libmysqlclient-dev', rhel: 'mysql-devel'
     end
   end
