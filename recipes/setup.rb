6e299d80b8c306aea653fa8d011cde3e9918cf53
Igor Rzegocki <igor@rzegocki.pl>
2017-05-30 19:55:40 +0200

    fix(ubuntu): proper provisioning for ubuntu 16.04
    Fixes #81

--- recipes/setup.rb
+++ recipes/setup.rb
@@ -7,6 +7,20 @@
 
 prepare_recipe
 
+# Monit and cleanup
+if node['platform_family'] == 'debian'
+  execute 'mkdir -p /etc/monit/conf.d'
+
+  file '/etc/monit/conf.d/00_httpd.monitrc' do
+    content "set httpd port 2812 and\n    use address localhost\n    allow localhost"
+  end
+
+  apt_package 'javascript-common' do
+    action :purge
+  end
+end
+
+
 # Ruby and bundler
 include_recipe 'deployer'
 if node['platform_family'] == 'debian'
