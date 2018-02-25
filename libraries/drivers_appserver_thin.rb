4505890247d0350e168348d7a7a1c4494be35a10
Igor Rzegocki <ajgon@users.noreply.github.com>
2018-02-25 15:16:58 +0100

    fix: failed to start appserver. (#146)
    ex)
    $ /srv/www/rails/shared/scripts/puma.service start
    Set Puma process UID to 5000
    Starting Puma rails
    cd /srv/www/rails/current && /usr/local/bin/bundle exec puma -C #{ROOT_PATH}/shared/config/puma.rb
    /srv/www/rails/shared/vendor/bundle/ruby/2.4.0/gems/puma-3.10.0/lib/puma/cli.rb:47:in `initialize': missing argument: -C (OptionParser::MissingArgument)
    	from /srv/www/rails/shared/vendor/bundle/ruby/2.4.0/gems/puma-3.10.0/bin/puma:8:in `new'
    	from /srv/www/rails/shared/vendor/bundle/ruby/2.4.0/gems/puma-3.10.0/bin/puma:8:in `<top (required)>'
    	from /srv/www/rails/shared/vendor/bundle/ruby/2.4.0/bin/puma:23:in `load'
    	from /srv/www/rails/shared/vendor/bundle/ruby/2.4.0/bin/puma:23:in `<main>'

--- libraries/drivers_appserver_thin.rb
+++ libraries/drivers_appserver_thin.rb
@@ -12,7 +12,7 @@ module Drivers
       end
 
       def appserver_command
-        'thin -C #\{ROOT_PATH\}/shared/config/thin.yml'
+        'thin -C #{ROOT_PATH}/shared/config/thin.yml' # rubocop:disable Lint/InterpolationCheck
       end
     end
   end
