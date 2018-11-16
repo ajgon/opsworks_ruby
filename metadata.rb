a2d4a8dbbd0e5a2ca3c28c49b3cd80c0e40c8d9c
Kevin Olbrich <22176+olbrich@users.noreply.github.com>
2018-11-16 04:34:42 -0500

    fix: Lock the windows cookbook dependency to maintain chef 12 compatibility (#196)

--- metadata.rb
+++ metadata.rb
@@ -18,6 +18,8 @@ depends 'sudo'
 
 # indirect dependency, but breaks against the chef_version if updated to 3.1.0
 depends 'seven_zip', '~> 2.0'
+# indirect dependency required to maintain compatibility with chef 12
+depends 'windows', '< 5.0'
 
 supports 'amazon', '>= 2017.03'
 supports 'ubuntu', '>= 16.04'
