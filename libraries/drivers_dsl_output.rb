0bde7fab35b9a2c9f0d3de5db12b67c6bf967fe5
Rich Seviora <richard.seviora@gmail.com>
2019-03-03 13:17:49 -0800

    fix Align SSL directory between template & driver
    - Webserver templates now get the SSL certificate path from driver options.

--- libraries/drivers_dsl_output.rb
+++ libraries/drivers_dsl_output.rb
@@ -2,6 +2,22 @@
 
 module Drivers
   module Dsl
+    # Generates output for template consumption from OpsWorks Config JSON.
+    #
+    # Each driver namespace (webserver, appserver, framework etc.) can have it's own set of attributes which can be
+    # set in OpsWorks Config JSON - for example: config['deploy']['appname']['webserver']['force_ssl'].
+    #
+    # To consume, add an output call to the class definition:
+    # <tt>
+    # class ::Drivers::Webserver::Apache < ::Drivers::Webserver::Base
+    #   # Filtering the output contents (recommended).
+    #   output filter:  %i[dhparams keepalive_timeout]
+    #   # Without filtering the output contents.
+    #   output
+    # end
+    # </tt>
+    #
+    # Output hashes should only include values that can be configured with JSON.
     module Output
       def self.included(klass)
         klass.instance_eval do
