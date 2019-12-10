62cda64e515481b25818fe57998670500b2f5ee4
kazukifujii <fujiiqz@gmail.com>
2019-12-10 16:58:45 +0900

    chore: add new webserver_config_params

--- libraries/drivers_appserver_passenger.rb
+++ libraries/drivers_appserver_passenger.rb
@@ -5,7 +5,7 @@ module Drivers
     class Passenger < Drivers::Appserver::Base
       adapter :passenger
       allowed_engines :passenger
-      output filter: %i[max_pool_size min_instances mount_point]
+      output filter: %i[max_pool_size min_instances mount_point pool_idle_time max_request_queue_size error_document]
 
       def manual_action(action); end
 
@@ -17,7 +17,7 @@ module Drivers
 
       def webserver_config_params
         o = out
-        Hash[%i[max_pool_size min_instances mount_point].map { |k| [k, o[k]] }].reject { |_k, v| v.nil? }
+        Hash[%i[max_pool_size min_instances mount_point pool_idle_time max_request_queue_size error_document].map { |k| [k, o[k]] }].reject { |_k, v| v.nil? }
       end
     end
   end
